81cea108a7a3123c1eda4672589a71991bb33b53
Ahtisham20 <ahtishamk05@gmail.com>
2023-12-28 23:18:33 +0500

    my first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var cxName = document.getElementById("cxName");
+var showCxName = document.getElementById("showCxName");
+var enterAppDate = document.getElementById("enterAppDate");
+var showenterAppDate = document.getElementById("showenterAppDate");
+var appTime = document.getElementById("appTime");
+var showappTime = document.getElementById("showappTime");
+var houseSize = document.getElementById("houseSize");
+var showhouseSize = document.getElementById("showhouseSize");
+var enterMobileNumber = document.getElementById("enterMobileNumber");
+var showenterMobileNumber = document.getElementById("showenterMobileNumber");
+var gender = document.getElementById("gender");
+var showgender = document.getElementById("showgender");
+var address = document.getElementById("address");
+var showaddress = document.getElementById("showaddress");
+var pricing = document.getElementById("pricing");
+var showpricing = document.getElementById("showpricing");
+var agentName = document.getElementById("agentName");
+var showagentName = document.getElementById("showagentName");
+var closerName = document.getElementById("closerName");
+var showcloserName = document.getElementById("showcloserName");
+var getData = document.getElementById("getData");
+var ShowData = document.getElementById("showData");
+
+
+function submitDkjklfjweljklrata() {
+ showCxName.innerHTML = cxName.value 
+ showenterAppDate.innerHTML = enterAppDate.value 
+ showappTime.innerHTML = appTime.value 
+ showhouseSize.innerHTML = houseSize.value 
+ showenterMobileNumber.innerHTML = enterMobileNumber.value 
+ showgender.innerHTML = gender.value
+ showaddress.innerHTML = address.value
+ showpricing.innerHTML = pricing.value
+ showagentName.innerHTML = agentName.value
+ showcloserName.innerHTML = closerName.value
+ console.log(getData);
+    getData.classList.add("sjkfhskfhsdjkdfh")
+    ShowData.classList.remove("sjkfhskfhsdjkdfh")
+    // return.submitDkjklfjweljklrata
+}
